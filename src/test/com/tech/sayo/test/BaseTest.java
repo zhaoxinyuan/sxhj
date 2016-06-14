10ae9b8f89ee91f86d5e410d06032f1c8b3032bb
zo <229731470@qq.com>
2016-06-14 19:28:53 +0800

    1

--- src/test/com/tech/sayo/test/BaseTest.java
+++ src/test/com/tech/sayo/test/BaseTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -19,31 +20,31 @@ import org.springframework.web.context.WebApplicationContext;
  * @author zo
  * @date 2016-06-14 10:28:33
  * @Description Spring Junit 测试基类,所有的测试类，都应继承该类
- * @annotations RunWith 以spring环境运行Junit,WebAppConfiguration 使用测试环境的ApplicationContext,ContextConfiguration 加载配置文件
- * */
+ * @annotations RunWith 以spring环境运行Junit,WebAppConfiguration
+ *              使用测试环境的ApplicationContext,ContextConfiguration
+ *              加载配置文件,TransactionConfiguration 事物回滚
+ */
 
-@RunWith(SpringJUnit4ClassRunner.class) 
-@WebAppConfiguration 
-@ContextConfiguration({
-	"file:src/main/resources/spring-mvc.xml",
-	"file:src/main/resources/spring-mybatis.xml"  
-})
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "file:src/main/resources/spring-mvc.xml", "file:src/main/resources/spring-mybatis.xml" })
+@TransactionConfiguration(defaultRollback = true)
 public class BaseTest {
 
 	@Autowired
-	private WebApplicationContext wac;  
-	public MockMvc mockMvc;  
+	private WebApplicationContext wac;
+	public MockMvc mockMvc;
 
 	/**
 	 * @Description 初始化和模拟request,response
 	 * @annotations @Before 在@test方法前执行
-	 * */
+	 */
 	@Before
 	public void setUp() {
-		this.mockMvc = webAppContextSetup(this.wac).build(); 
+		this.mockMvc = webAppContextSetup(this.wac).build();
 	}
-	
-	public void PrintResponseBody(MvcResult result){
+
+	public void PrintResponseBody(MvcResult result) {
 		try {
 			System.out.println("ResponseBody:");
 			System.out.println(URLDecoder.decode("	" + result.getResponse().getContentAsString(), "utf-8") + "\n");
--- src/test/com/tech/sayo/test/CleanActionTest.java
+++ src/test/com/tech/sayo/test/CleanActionTest.java
@@ -1,24 +1,32 @@
 package com.tech.sayo.test;
- 
+
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
- 
+
+import com.google.gson.GsonBuilder;
+import com.tech.sayo.wechat.clean.bean.CleOrder;
+import com.tech.sayo.wechat.clean.bean.CleOrderDetail;
+import com.tech.sayo.wechat.clean.bean.NanOrder;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 /**
  * @author zo
  * @date 2016-06-14 13:11:55
  * @Description 测试  controller CleanAction
- * */
+ * */  
 public class CleanActionTest extends BaseTest {
 	/**
 	 * @Description 测试 url cleanAction/categories 打印返回结果
 	 * @annotations Test 被Junit 执行的测试方法
 	 * */
-	@Test
-	public void testAmount() {
+	//@Test
+	public void testCategories() {
 		System.out.println("\n" + "************************************************* Test cleanAction/categories *************************************************");
 		try {
 			MvcResult result = super.mockMvc.perform((get("/cleanAction/categories").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
@@ -28,5 +36,154 @@ public class CleanActionTest extends BaseTest {
 		}  
 	}
 	
+	//@Test
+	public void testCategory() {
+		System.out.println("\n" + "************************************************* Test cleanAction/category *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/categories").param("categoryId", "1").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
 	
+	//@Test
+	public void testStaffs() {
+		System.out.println("\n" + "************************************************* Test cleanAction/staffs *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/staffs").param("categoryId", "1").param("current", "1").param("rowCount", "10").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testStaff() {
+		System.out.println("\n" + "************************************************* Test cleanAction/staff *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/staff").param("staffId", "1").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testCleordersubmit() {
+		System.out.println("\n" + "************************************************* Test cleanAction/cleordersubmit *************************************************");
+		CleOrder order = new CleOrder();
+		order.setOrderCategoryId(1);
+		order.setOrderRealpayamount(60D);
+		order.setOrderPayableamount(60D);
+		order.setOrderPaytype(0);
+		order.setOrderUserid(1);
+		order.setOrderAddressid(221);
+		
+		List<CleOrderDetail> list = new ArrayList<CleOrderDetail>();
+		
+		CleOrderDetail detail = new CleOrderDetail();
+		detail.setDetaiAmount(60D);
+		detail.setDetailServicePrice(30D);
+		detail.setDetailServiceHours(2);
+		detail.setDetailServiceFrom("2016-06-14 18:00");
+		detail.setDetailServiceTo("2016-06-14 20:00");
+		detail.setDetailType(0);
+		
+		list.add(detail);
+		order.setOrderDetail(list);
+		String jsonStr = new GsonBuilder().disableHtmlEscaping().create().toJson(order);
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/cleordersubmit").param("order", jsonStr).param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testNanordersubmit() {
+		System.out.println("\n" + "************************************************* Test cleanAction/nanordersubmit *************************************************");
+		
+		NanOrder order = new NanOrder();
+		order.setOrderCategoryid(4);
+		order.setOrderStaffid(4);
+		order.setOrderInterviewtime("2016-06-14 18:00");
+		order.setOrderUserid(1);
+		order.setOrderUseraddressid(221);
+		
+		String jsonStr = new GsonBuilder().disableHtmlEscaping().create().toJson(order);
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/nanordersubmit").param("order", jsonStr).param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testCleorders() {
+		System.out.println("\n" + "************************************************* Test cleanAction/cleorders *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/cleorders").param("orderUserid", "1").param("current", "1").param("rowCount", "10").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void tetsCleorder() {
+		System.out.println("\n" + "************************************************* Test cleanAction/cleorder *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/cleorder").param("orderId", "93").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testNanorders() {
+		System.out.println("\n" + "************************************************* Test cleanAction/nanorders *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/nanorders").param("orderUserid", "1").param("current", "1").param("rowCount", "10").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testNanorder() {
+		System.out.println("\n" + "************************************************* Test cleanAction/nanorder *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/nanorder").param("orderId", "3").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	//@Test
+	public void testStafftimes() {
+		System.out.println("\n" + "************************************************* Test cleanAction/stafftimes *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/stafftimes").param("staffId", "1").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	@Test
+	public void testCancleCleOrder() {
+		System.out.println("\n" + "************************************************* Test cleanAction/stafftimes *************************************************");
+		try {
+			MvcResult result = super.mockMvc.perform((get("/cleanAction/canclecleorder").param("orderId", "93").param("callback", "?").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk()).andDo(print()).andReturn();
+			super.PrintResponseBody(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
 }
